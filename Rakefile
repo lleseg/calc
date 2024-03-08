da98cb921be3729e7f5416b56dec0e744ae0b84a
Franco De Nardo <fdenardo.utn@gmail.com>
2024-03-08 18:38:55 -0300

    feat: :sparkles: basic sum calc

---
+++ Rakefile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+task default: %w[test]
+
+task :run do
+  ruby 'lib/calc.rb'
+end
+
+task :test do
+  ruby 'spec/calc_test.rb'
+end
---
+++ lib/calc.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+print "Ingresá el primer número: "
+n1 = gets.chomp.to_i
+print "Ingresá el segundo número: "
+n2 = gets.chomp.to_i
+
+suma = n1 + n2
+puts "La suma de los dos números es: #{suma}"
